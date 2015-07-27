0d30c34a739ce833c6c296c7e9da870fb316f042
Jonathan Barnes <jbarnes@pivotal.io>
2015-07-27 10:37:36 -0700

    Removes old .yml* version
    Also removes temporary spec examples that supported converting from
    yml->json.
    
    [#95551478](https://www.pivotaltracker.com/story/show/95551478)
    
    Signed-off-by: Rexxar Liang (Biao Liang) <rexxar.liang@huawei.com>

--- spec/helpers/renderer.rb
+++
@@ -1,9 +0,0 @@
-require 'JSON'
-
-class Renderer
-  def self.render(spec, template_name)
-    evaluation_context = EvaluationContext.new(spec)
-    template = ERB.new(File.read(template_name))
-    template.result(evaluation_context.get_binding)
-  end
-end
