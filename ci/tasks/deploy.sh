8aa60036100e969de67979e665ccb1bc62257cd0
Aaron Hurley <ahurley@pivotal.io>
2015-08-03 17:45:09 -0700

    Updated references to terraform state version and export files
    We are removing the duplication of ubuntu/centos references to simplify
    the pipeline.
    
    Also, in deploy job we have updated the relative paths to use absolute
    paths.
    
    [#99271662](https://www.pivotaltracker.com/story/show/99271662)
    
    Signed-off-by: Edwin Xie <exie@pivotal.io>

--- ci/tasks/deploy.sh
+++ ci/tasks/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -e -x
+set -e
 
 source bosh-cpi-release/ci/tasks/utils.sh
 
@@ -11,28 +11,30 @@ check_param aws_secret_access_key
 source /etc/profile.d/chruby.sh
 chruby 2.1.2
 
-cpi_release_name=bosh-aws-cpi
 semver=`cat version-semver/number`
-manifest_dir=bosh-concourse-ci/pipelines/$cpi_release_name
+cpi_release_name=bosh-aws-cpi
+working_dir=$PWD
+manifest_dir=${working_dir}/bosh-concourse-ci/pipelines/$cpi_release_name
 manifest_filename=${manifest_dir}/${base_os}-director-manifest.yml
 
 terraform_statefile_semver=`cat terraform-state-version/number`
-source terraform-${base_os}-exports/terraform-${base_os}-exports-${terraform_statefile_semver}.sh
+source terraform-exports/terraform-${base_os}-exports-${terraform_statefile_semver}.sh
 
 # leave this for now, it will be heredoc'ed in future
 ./bosh-concourse-ci/tasks/generate-manifest.rb ${manifest_filename}
 
 echo "normalizing paths to match values referenced in $manifest_filename"
-mkdir ./tmp
-cp ./bosh-cpi-dev-artifacts/${cpi_release_name}-${semver}.tgz ./tmp/${cpi_release_name}.tgz
-cp ./bosh-release/release.tgz ./tmp/bosh-release.tgz
-cp ./stemcell/stemcell.tgz ./tmp/stemcell.tgz
-cp ./bosh-concourse-ci/pipelines/bosh-aws-cpi/bats.pem ./tmp/bats.pem
+# manifest paths are now relative so the tmp inputs need to be updated
+mkdir ${manifest_dir}/tmp
+cp ./bosh-cpi-dev-artifacts/${cpi_release_name}-${semver}.tgz ${manifest_dir}/tmp/${cpi_release_name}.tgz
+cp ./bosh-release/release.tgz ${manifest_dir}/tmp/bosh-release.tgz
+cp ./stemcell/stemcell.tgz ${manifest_dir}/tmp/stemcell.tgz
+cp ./bosh-concourse-ci/pipelines/bosh-aws-cpi/bats.pem ${manifest_dir}/tmp/bats.pem
 
 initver=$(cat bosh-init/version)
 initexe="$PWD/bosh-init/bosh-init-${initver}-linux-amd64"
 chmod +x $initexe
-chmod 400 ./tmp/bats.pem
+chmod 400 ${manifest_dir}/tmp/bats.pem
 
 echo "using bosh-init CLI version..."
 $initexe version
