90386406f9481b04cb8af4c2942a50901c79227e
Aaron Hurley <ahurley@pivotal.io>
2015-08-05 15:29:46 -0400

    Added new s3 version buckets for terraform state and exports files
    [#99573896](https://www.pivotaltracker.com/story/show/99573896)
    
    Signed-off-by: Scott Weiss <scott.weiss@emc.com>

--- ci/tasks/terraform.sh
+++ ci/tasks/terraform.sh
@@ -9,18 +9,16 @@ check_param aws_secret_access_key
 check_param concourse_ip
 check_param base_os
 
-semver=`cat terraform-state-version/number`
-
 # generates a plan
 /terraform/terraform plan -out=${base_os}-bats.tfplan \
   -var "access_key=${aws_access_key_id}" \
   -var "secret_key=${aws_secret_access_key}" \
-  -var "build_id=bats-${base_os}-${semver}" \
+  -var "build_id=bats-${base_os}" \
   -var "concourse_ip=${concourse_ip}" \
   bosh-concourse-ci/pipelines/bosh-aws-cpi
 
-state_file=${base_os}-bats-${semver}.tfstate
-export_file=terraform-${base_os}-exports-${semver}.sh
+state_file=${base_os}-bats.tfstate
+export_file=terraform-${base_os}-exports.sh
 
 # applies the plan, generates a state file
 /terraform/terraform apply -state=$state_file ${base_os}-bats.tfplan
