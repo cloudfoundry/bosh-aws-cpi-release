227f22c18b51e76816d48b15b08a0eb10395de4d
Aaron Hurley <ahurley@pivotal.io>
2015-08-06 13:09:30 -0400

    TF Vars in separate file
    [#99573896](https://www.pivotaltracker.com/story/show/99573896)
    
    Signed-off-by: Scott Weiss <scott.weiss@emc.com>

--- ci/tasks/terraform.sh
+++ ci/tasks/terraform.sh
@@ -11,30 +11,27 @@ check_param base_os
 check_param security_group_trusted_ip
 
 #heredoc variables.tf
-cat > "variables.tf" <<EOF
-variable "access_key" {
-  default = "${aws_access_key_id}"
-}
-variable "secret_key" {
-  default = "${aws_secret_access_key}"
-}
-variable "build_id" {
-  default = "bats-${base_os}"
-}
-variable "concourse_ip" {
-  default = "${concourse_ip}"
-}
-variable "security_group_trusted_ip" {
-  default = "${security_group_trusted_ip}"
-}
+cat > "terraform.tfvars" <<EOF
+access_key = "${aws_access_key_id}"
+secret_key = "${aws_secret_access_key}"
+build_id = "bats-${base_os}"
+concourse_ip = "${concourse_ip}"
+security_group_trusted_ip = "${security_group_trusted_ip}"
 EOF
 
 #heredoc .tf config
 cat > "temp_tf_config.tf" <<'EOF'
 
+variable "access_key" {}
+variable "secret_key" {}
+variable "build_id" {}
+variable "concourse_ip" {}
+variable "security_group_trusted_ip" {}
+
+
 provider "aws" {
-    access_key = "\${var.access_key}"
-    secret_key = "\${var.secret_key}"
+    access_key = "${var.access_key}"
+    secret_key = "${var.secret_key}"
     region = "us-east-1"
 }
 
@@ -42,48 +39,48 @@ resource "aws_vpc" "vpc" {
     cidr_block = "10.0.0.0/16"
 
     tags {
-        Name = "\${var.build_id}"
+        Name = "${var.build_id}"
     }
 }
 
 resource "aws_internet_gateway" "gw" {
-    vpc_id = "\${aws_vpc.vpc.id}"
+    vpc_id = "${aws_vpc.vpc.id}"
 
     tags {
-        Name = "\${var.build_id}"
+        Name = "${var.build_id}"
     }
 }
 
 resource "aws_route_table" "rt" {
-    vpc_id = "\${aws_vpc.vpc.id}"
+    vpc_id = "${aws_vpc.vpc.id}"
     route {
         cidr_block = "0.0.0.0/0"
-        gateway_id = "\${aws_internet_gateway.gw.id}"
+        gateway_id = "${aws_internet_gateway.gw.id}"
     }
 
     tags {
-        Name = "\${var.build_id}"
+        Name = "${var.build_id}"
     }
 }
 
 resource "aws_subnet" "sn" {
-    vpc_id = "\${aws_vpc.vpc.id}"
+    vpc_id = "${aws_vpc.vpc.id}"
     cidr_block = "10.0.0.0/24"
 
     tags {
-        Name = "\${var.build_id}"
+        Name = "${var.build_id}"
     }
 }
 
 resource "aws_route_table_association" "rta" {
-    subnet_id = "\${aws_subnet.sn.id}"
-    route_table_id = "\${aws_route_table.rt.id}"
+    subnet_id = "${aws_subnet.sn.id}"
+    route_table_id = "${aws_route_table.rt.id}"
 }
 
 resource "aws_security_group" "bats_sg" {
-  name = "bats_sg-\${var.build_id}"
+  name = "bats_sg-${var.build_id}"
   description = "allows local and concourse traffic"
-  vpc_id = "\${aws_vpc.vpc.id}"
+  vpc_id = "${aws_vpc.vpc.id}"
 
   ingress {
       from_port = 0
@@ -96,42 +93,42 @@ resource "aws_security_group" "bats_sg" {
       from_port = 22
       to_port = 22
       protocol = "tcp"
-      cidr_blocks = ["\${var.concourse_ip}/32"]
+      cidr_blocks = ["${var.concourse_ip}/32"]
   }
 
   ingress {
       from_port = 22
       to_port = 22
       protocol = "tcp"
-      cidr_blocks = ["\${var.security_group_trusted_ip}/32"]
+      cidr_blocks = ["${var.security_group_trusted_ip}/32"]
   }
 
   ingress {
       from_port = 6868
       to_port = 6868
       protocol = "tcp"
-      cidr_blocks = ["\${var.concourse_ip}/32"]
+      cidr_blocks = ["${var.concourse_ip}/32"]
   }
 
   ingress {
       from_port = 6868
       to_port = 6868
       protocol = "tcp"
-      cidr_blocks = ["\${var.security_group_trusted_ip}/32"]
+      cidr_blocks = ["${var.security_group_trusted_ip}/32"]
   }
 
   ingress {
       from_port = 25555
       to_port = 25555
       protocol = "tcp"
-      cidr_blocks = ["\${var.concourse_ip}/32"]
+      cidr_blocks = ["${var.concourse_ip}/32"]
   }
 
   ingress {
       from_port = 25555
       to_port = 25555
       protocol = "tcp"
-      cidr_blocks = ["\${var.security_group_trusted_ip}/32"]
+      cidr_blocks = ["${var.security_group_trusted_ip}/32"]
   }
 
   ingress {
@@ -149,7 +146,7 @@ resource "aws_security_group" "bats_sg" {
   }
 
   tags {
-    Name = "\${var.build_id}"
+    Name = "${var.build_id}"
   }
 }
 
@@ -162,23 +159,23 @@ resource "aws_eip" "bats_vip" {
 }
 
 output "director_vip" {
-    value = "\${aws_eip.director_vip.public_ip}"
+    value = "${aws_eip.director_vip.public_ip}"
 }
 
 output "bats_vip" {
-    value = "\${aws_eip.bats_vip.public_ip}"
+    value = "${aws_eip.bats_vip.public_ip}"
 }
 
 output "subnet_id" {
-    value = "\${aws_subnet.sn.id}"
+    value = "${aws_subnet.sn.id}"
 }
 
 output "security_group_name" {
-    value = "\${aws_security_group.bats_sg.name}"
+    value = "${aws_security_group.bats_sg.name}"
 }
 
 output "availability_zone" {
-    value = "\${aws_subnet.sn.availability_zone}"
+    value = "${aws_subnet.sn.availability_zone}"
 }
 EOF
 
