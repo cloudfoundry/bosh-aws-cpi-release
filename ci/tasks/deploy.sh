d99432d0a7f0867ff100e4cee8fe7d53aefa54c5
Scott Weiss <scott.weiss@emc.com>
2015-08-06 15:04:10 -0400

    Extract director manifest to heredoc in deploy task
    [#99573896](https://www.pivotaltracker.com/story/show/99573896)
    
    Signed-off-by: Aaron Hurley <ahurley@pivotal.io>

--- ci/tasks/deploy.sh
+++ ci/tasks/deploy.sh
@@ -3,10 +3,15 @@
 set -e
 
 source bosh-cpi-release/ci/tasks/utils.sh
+source terraform-exports/terraform-${base_os}-exports.sh
 
 check_param base_os
 check_param aws_access_key_id
 check_param aws_secret_access_key
+check_param SUBNET_ID
+check_param AVAILABILITY_ZONE
+check_param DIRECTOR
+check_param SECURITY_GROUP_NAME
 
 source /etc/profile.d/chruby.sh
 chruby 2.1.2
@@ -17,10 +22,153 @@ working_dir=$PWD
 manifest_dir=${working_dir}/bosh-concourse-ci/pipelines/$cpi_release_name
 manifest_filename=${manifest_dir}/${base_os}-director-manifest.yml
 
-source terraform-exports/terraform-${base_os}-exports.sh
+#create director manifest as heredoc
+cat > "${manifest_filename}"<<EOF
+---
+name: bosh
 
-# leave this for now, it will be heredoc'ed in future
-./bosh-concourse-ci/tasks/generate-manifest.rb ${manifest_filename}
+releases:
+- name: bosh
+  url: file://tmp/bosh-release.tgz
+- name: bosh-aws-cpi
+  url: file://tmp/bosh-aws-cpi.tgz
+
+networks:
+- name: private
+  type: manual
+  subnets:
+  - range:    10.0.0.0/24
+    gateway:  10.0.0.1
+    dns:      [8.8.8.8]
+    cloud_properties: {subnet: $SUBNET_ID}
+- name: public
+  type: vip
+
+resource_pools:
+- name: default
+  network: private
+  stemcell:
+    url: file://tmp/stemcell.tgz
+  cloud_properties:
+    instance_type: m3.xlarge
+    availability_zone: $AVAILABILITY_ZONE
+    ephemeral_disk:
+      size: 25000
+      type: gp2
+
+disk_pools:
+- name: default
+  disk_size: 25_000
+  cloud_properties: {type: gp2}
+
+jobs:
+- name: bosh
+  templates:
+  - {name: nats, release: bosh}
+  - {name: redis, release: bosh}
+  - {name: postgres, release: bosh}
+  - {name: blobstore, release: bosh}
+  - {name: director, release: bosh}
+  - {name: health_monitor, release: bosh}
+  - {name: registry, release: bosh}
+  - {name: cpi, release: bosh-aws-cpi}
+
+  instances: 1
+  resource_pool: default
+  persistent_disk_pool: default
+
+  networks:
+  - name: private
+    static_ips: [10.0.0.6]
+    default: [dns, gateway]
+  - name: public
+    static_ips: [$DIRECTOR]
+
+  properties:
+    nats:
+      address: 127.0.0.1
+      user: nats
+      password: nats-password
+
+    redis:
+      listen_addresss: 127.0.0.1
+      address: 127.0.0.1
+      password: redis-password
+
+    postgres: &db
+      host: 127.0.0.1
+      user: postgres
+      password: postgres-password
+      database: bosh
+      adapter: postgres
+
+    # Tells the Director/agents how to contact registry
+    registry:
+      address: 10.0.0.6
+      host: 10.0.0.6
+      db: *db
+      http: {user: admin, password: admin, port: 25777}
+      username: admin
+      password: admin
+      port: 25777
+
+    # Tells the Director/agents how to contact blobstore
+    blobstore:
+      address: 10.0.0.6
+      port: 25250
+      provider: dav
+      director: {user: director, password: director-password}
+      agent: {user: agent, password: agent-password}
+
+    director:
+      address: 127.0.0.1
+      name: micro
+      db: *db
+      cpi_job: cpi
+
+    hm:
+      http: {user: hm, password: hm-password}
+      director_account: {user: admin, password: admin}
+
+    aws: &aws
+      access_key_id: $aws_access_key_id
+      secret_access_key: $aws_secret_access_key
+      default_key_name: "bats"
+      default_security_groups: [$SECURITY_GROUP_NAME]
+      region: "us-east-1"
+
+    # Tells agents how to contact nats
+    agent: {mbus: "nats://nats:nats-password@10.0.0.6:4222"}
+
+    ntp: &ntp
+    - 0.north-america.pool.ntp.org
+    - 1.north-america.pool.ntp.org
+
+cloud_provider:
+  template: {name: cpi, release: bosh-aws-cpi}
+
+  # Tells bosh-micro how to SSH into deployed VM
+  ssh_tunnel:
+    host: $DIRECTOR
+    port: 22
+    user: vcap
+    private_key: tmp/bats.pem
+
+  # Tells bosh-micro how to contact remote agent
+  mbus: https://mbus-user:mbus-password@$DIRECTOR:6868
+
+  properties:
+    aws: *aws
+
+    # Tells CPI how agent should listen for requests
+    agent: {mbus: "https://mbus-user:mbus-password@0.0.0.0:6868"}
+
+    blobstore:
+      provider: local
+      path: /var/vcap/micro_bosh/data/cache
+
+    ntp: *ntp
+EOF
 
 echo "normalizing paths to match values referenced in $manifest_filename"
 # manifest paths are now relative so the tmp inputs need to be updated
