a6b0ea317982d31527c10483a7e6664cb0eef1c6
Scott Weiss <scott.weiss@emc.com>
2015-08-12 11:10:50 -0400

    Terraform job destroys instance and infrastructure before running
    If an existing director from previous deploy job exists, it will be terminated.
    Terraform destroy will then be called. Terraform job now imports state file from
    previous terraform, along with director state manifest from deploy job to make
    manual calls to aws cli.
    
    [#99573316](https://www.pivotaltracker.com/story/show/99573316)
    
    Signed-off-by: Aaron Hurley <ahurley@pivotal.io>

--- ci/tasks/terraform.sh
+++ ci/tasks/terraform.sh
@@ -6,7 +6,40 @@ source bosh-cpi-release/ci/tasks/utils.sh
 
 check_param aws_access_key_id
 check_param aws_secret_access_key
+check_param region_name
 check_param base_os
+
+export AWS_ACCESS_KEY_ID=${aws_access_key_id}
+export AWS_SECRET_ACCESS_KEY=${aws_secret_access_key}
+export AWS_DEFAULT_REGION=${region_name}
+
+apt-get update
+apt-get -y install python
+curl -O https://bootstrap.pypa.io/get-pip.py
+python get-pip.py
+apt-get -y install groff
+apt-get -y install jq
+pip install awscli
+
+#tear down previous director if it exists
+previous_director_instance=$(jq '.current_vm_cid' director-state-file/${base_os}-director-manifest-state.json)
+previous_security_group_id=$(jq '.modules[].resources["aws_security_group.bats_sg"].primary.id' ubuntu-bats.tfstate)
+set +e
+
+aws ec2 terminate-instances --instance-ids ${previous_director_instance//\"/}
+aws ec2 delete-security-group --group-id ${previous_security_group_id//\"/}
+set -e
+
+desired_status="\"terminated\""
+instance_status=$(aws ec2 describe-instance-status --instance-id i-1fc65ff6 | jq '.InstanceStatuses[].InstanceState.Name')
+if [ -v "$instance_status" ]; then
+  while [ "$desired_status" != "$instance_status" ]
+  do
+    instance_status=$(aws ec2 describe-instance-status --instance-id i-1fc65ff6 | jq '.InstanceStatuses[].InstanceState.Name')
+    echo "pausing 20s for instance termination..."
+    sleep 20s
+  done
+fi
 
 #heredoc variables.tf
 cat > "terraform.tfvars" <<EOF
@@ -16,16 +49,22 @@ build_id = "bats-${base_os}"
 EOF
 
 #copy tf file from assets to working directory
-cp bosh-cpi-release/ci/assets/bosh-workspace.tf ./bosh-workspace.tf
-
-# generates a plan
-/terraform/terraform plan -out=${base_os}-bats.tfplan
+cp bosh-cpi-release/ci/assets/bosh-workspace.tf bosh-workspace.tf
 
 state_file=${base_os}-bats.tfstate
 export_file=terraform-${base_os}-exports.sh
+tf_plan_file=${base_os}-bats.tfplan
+
+#copy existing state file to working directory
+cp terraform-state/${state_file} ${state_file}
+
+/terraform/terraform destroy -force -state=${state_file}
+
+# generates a plan
+/terraform/terraform plan -out=${tf_plan_file}
 
 # applies the plan, generates a state file
-/terraform/terraform apply -state=$state_file ${base_os}-bats.tfplan
+/terraform/terraform apply -state=${state_file} ${tf_plan_file}
 
 # exports values into an exports file
 echo -e "#!/usr/bin/env bash" >> $export_file
