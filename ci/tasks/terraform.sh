faf251f627bf54292630f3b0b311793c16009720
Xuebin He <xuebin.he@emc.com>
2015-08-17 17:16:42 -0400

    Terraform job no longer cleans up infrastructure from previous terraform
    [#101427994](https://www.pivotaltracker.com/story/show/101427994)
    
    Signed-off-by: Scott Weiss <scott.weiss@emc.com>

--- ci/tasks/terraform.sh
+++ ci/tasks/terraform.sh
@@ -6,51 +6,7 @@ source bosh-cpi-release/ci/tasks/utils.sh
 
 check_param aws_access_key_id
 check_param aws_secret_access_key
-check_param region_name
 check_param base_os
-
-export AWS_ACCESS_KEY_ID=${aws_access_key_id}
-export AWS_SECRET_ACCESS_KEY=${aws_secret_access_key}
-export AWS_DEFAULT_REGION=${region_name}
-
-apt-get update
-apt-get -y install python
-curl -O https://bootstrap.pypa.io/get-pip.py
-python get-pip.py
-apt-get -y install groff
-apt-get -y install jq
-pip install awscli
-
-#copy tf file from assets to working directory
-cp bosh-cpi-release/ci/assets/bosh-workspace.tf bosh-workspace.tf
-
-state_file=${base_os}-bats.tfstate
-export_file=terraform-${base_os}-exports.sh
-tf_plan_file=${base_os}-bats.tfplan
-
-#copy existing state file to working directory
-cp terraform-state/${state_file} ${state_file}
-
-#tear down previous director if it exists
-previous_director_instance=$(jq '.current_vm_cid' director-state-file/${base_os}-director-manifest-state.json)
-previous_security_group_id=$(jq '.modules[].resources["aws_security_group.bats_sg"].primary.id' ${state_file})
-previous_director_instance=${previous_director_instance//\"/}
-set +e
-
-instance_status=$(aws ec2 terminate-instances --instance-ids ${previous_director_instance} | jq '.TerminatingInstances[].CurrentState.Name')
-
-desired_status="\"terminated\""
-timeout=0
-while [ "$desired_status" != "$instance_status" ] && [ ! -z "$instance_status" ]  &&  [ "$timeout" -lt 300 ]
-do
-  instance_status=$(aws ec2 terminate-instances --instance-ids ${previous_director_instance} | jq '.TerminatingInstances[].CurrentState.Name')
-  echo "pausing 20s for instance termination..."
-  sleep 20s
-  timeout=$((timeout + 20))
-done
-
-aws ec2 delete-security-group --group-id ${previous_security_group_id//\"/}
-set -e
 
 #heredoc variables.tf
 cat > "terraform.tfvars" <<EOF
@@ -59,26 +15,17 @@ secret_key = "${aws_secret_access_key}"
 build_id = "bats-${base_os}"
 EOF
 
-set +e
-/terraform/terraform destroy -force -state=${state_file}
-status=$?
-set -e
-#sometimes terraform is slow and needs to retry
-if [ "${status}" != "0" ]; then
-  /terraform/terraform destroy -force -state=${state_file}
-fi
+#copy tf file from assets to working directory
+cp bosh-cpi-release/ci/assets/bosh-workspace.tf ./bosh-workspace.tf
 
 # generates a plan
-/terraform/terraform plan -out=${tf_plan_file}
+/terraform/terraform plan -out=${base_os}-bats.tfplan
+
+state_file=${base_os}-bats.tfstate
+export_file=terraform-${base_os}-exports.sh
 
 # applies the plan, generates a state file
-set +e
-/terraform/terraform apply -state=${state_file} ${tf_plan_file}
-status=$?
-set -e
-if [ "${status}" != "0" ]; then
-  /terraform/terraform apply -state=${state_file} ${tf_plan_file}
-fi
+/terraform/terraform apply -state=$state_file ${base_os}-bats.tfplan
 
 # exports values into an exports file
 echo -e "#!/usr/bin/env bash" >> $export_file
