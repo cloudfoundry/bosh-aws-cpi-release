13eacf1ee569e34f7c2e5b507ecb2d605c118636
Xuebin He <xuebin.he@emc.com>
2015-08-25 10:26:23 -0400

    Add necessary inputs for teardown-director
    Fix error: Running command: 'tar --no-same-owner -xzvf
    /tmp/build/a9023a0c-662a-4448-7532-95164b2b464b/director-state-file/tmp/bo
    sh-aws-cpi.tgz -C /root/.bosh_init/installations/66ff190a-33ed-47c8-52dc-f
    37108da62d2/tmp/bosh-init-release402900959', stdout: '', stderr: 'tar (chi
    ld): /tmp/build/a9023a0c-662a-4448-7532-95164b2b464b/director-state-file/t
    mp/bosh-aws-cpi.tgz: Cannot open: No such file or directory
    
    [#101428374](https://www.pivotaltracker.com/story/show/101428374)
    
    Signed-off-by: Scott Weiss <scott.weiss@emc.com>

--- ci/tasks/teardown-director.sh
+++ ci/tasks/teardown-director.sh
@@ -20,6 +20,15 @@ initver=$(cat bosh-init/version)
 initexe="$PWD/bosh-init/bosh-init-${initver}-linux-amd64"
 chmod +x $initexe
 
+echo "normalizing paths to match values referenced in $manifest_filename"
+# manifest paths are now relative so the tmp inputs need to be updated
+mkdir ${manifest_dir}/tmp
+semver=$(cat version-semver/number)
+cp ./bosh-cpi-dev-artifacts/bosh-aws-cpi-${semver}.tgz ${manifest_dir}/tmp/bosh-aws-cpi.tgz
+cp ./bosh-release/release.tgz ${manifest_dir}/tmp/bosh-release.tgz
+cp ./stemcell/stemcell.tgz ${manifest_dir}/tmp/stemcell.tgz
+cp ./bosh-concourse-ci/pipelines/bosh-aws-cpi/bats.pem ${manifest_dir}/tmp/bats.pem
+
 echo "using bosh-init CLI version..."
 $initexe version
 
