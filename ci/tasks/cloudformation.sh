742cf17c4a362b12d4983ba483f11c26c832c903
Jonathan Barnes <jbarnes@pivotal.io>
2015-08-28 12:53:02 -0400

    Extract cloudfoumation describe calls to utils.sh
    This is in preparation to re-use these functions in other scripts
    
    [#101717890](https://www.pivotaltracker.com/story/show/101717890)
    
    Signed-off-by: Xuebin He <xuebin.he@emc.com>

--- ci/tasks/cloudformation.sh
+++ ci/tasks/cloudformation.sh
@@ -13,13 +13,11 @@ export AWS_SECRET_ACCESS_KEY=${aws_secret_access_key}
 export AWS_DEFAULT_REGION=${region_name}
 
 stack_name="aws-cpi-stack"
-describe_cmd="aws cloudformation describe-stacks --stack-name ${stack_name}"
 
 aws cloudformation delete-stack --stack-name ${stack_name}
 
 while true; do
-  if stack_info=$(${describe_cmd}); then
-    stack_status=$(echo $stack_info | jq '.Stacks[].StackStatus')
+  if stack_status=$(get_stack_status $stack_name); then
     if [ $stack_status != '"DELETE_IN_PROGRESS"' ]; then
       echo "Expecting the stack to either be deleted or in the process of being deleted but was ${stack_status}"
       echo ${stack_info}
@@ -37,20 +35,18 @@ aws cloudformation create-stack \
     --stack-name      ${stack_name} \
     --template-body   file:///${PWD}/bosh-cpi-release/ci/assets/cloudformation.template
 
-stack_status='"CREATE_IN_PROGRESS"'
 while true; do
+  stack_status=$(get_stack_status $stack_name)
   echo "StackStatus ${stack_status}"
   if [ $stack_status == '"CREATE_IN_PROGRESS"' ]; then
     echo "sleeping 5"
     sleep 5s
-    stack_info=$(${describe_cmd})
-    stack_status=$(echo $stack_info | jq '.Stacks[].StackStatus')
   else
     break
   fi
 done
 
 if [ $stack_status != '"CREATE_COMPLETE"' ]; then
-  echo "cloudformation failed stack info:\n${stack_info}"
+  echo "cloudformation failed stack info:\n$(get_stack_info $stack_name)"
   exit 1
 fi
--- ci/tasks/utils.sh
+++ ci/tasks/utils.sh
@@ -1,5 +1,28 @@
 #!/usr/bin/env bash
 
+get_stack_info() {
+  stack_name=$1
+  return $(aws cloudformation describe-stacks --stack-name ${stack_name})
+}
+
+get_stack_info_of() {
+  local base_os=$1
+  local key=$2
+  local stack_name=$3
+
+  stack_info=get_stack_info $stack_name
+  return $( echo ${stack_info} | \
+          jq --arg base_os ${base_os} --arg key ${key} \
+          '.Stacks[].Outputs[] | select(.OutputKey=="\($base_os)\($key)").OutputValue' )
+}
+
+get_stack_status() {
+  local stack_name=$1
+
+  stack_info=get_stack_info $stack_name
+  return $( echo ${stack_info} | jq '.Stacks[].StackStatus' )
+}
+
 check_param() {
   local name=$1
   local value=$(eval echo '$'$name)
