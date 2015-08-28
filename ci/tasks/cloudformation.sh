28dad2a0a001729ccc1f30859954e735bdc17806
Jonathan Barnes <jbarnes@pivotal.io>
2015-08-28 12:46:10 -0400

    Replace terraform jobs with single recreate-infrastructure job
    This job uses aws cloudformation instead of terraform as we like it's interface
    better.  Additionally we now attempt to delete a cloudformation stack prior to
    attempting to create one.
    
    [#101717890](https://www.pivotaltracker.com/story/show/101717890)
    
    Signed-off-by: Xuebin He <xuebin.he@emc.com>

--- ci/tasks/cloudformation.sh
+++ ci/tasks/cloudformation.sh
@@ -7,36 +7,48 @@ source bosh-cpi-release/ci/tasks/utils.sh
 check_param aws_access_key_id
 check_param aws_secret_access_key
 check_param region_name
-check_param base_os
 
 export AWS_ACCESS_KEY_ID=${aws_access_key_id}
 export AWS_SECRET_ACCESS_KEY=${aws_secret_access_key}
 export AWS_DEFAULT_REGION=${region_name}
 
-export_file=cloudformation-${base_os}-exports.sh
 stack_name="aws-cpi-stack"
+
+aws cloudformation delete-stack --stack-name ${stack_name}
+
+describe_cmd="aws cloudformation describe-stacks --stack-name ${stack_name}"
+
+while true; do
+  if stack_info=$(${describe_cmd}); then
+    stack_status=$(echo $stack_info | jq '.Stacks[].StackStatus')
+    if [ $stack_status != '"DELETE_IN_PROGRESS"' ]; then
+      echo "Expecting the stack to either be deleted or in the process of being deleted but was ${stack_status}"
+      echo ${stack_info}
+      exit 1
+    fi
+    echo "sleeping 5"
+    sleep 5s
+  else
+    break
+  fi
+done
 
 aws cloudformation create-stack \
     --stack-name      ${stack_name} \
     --template-body   file:///${PWD}/bosh-cpi-release/ci/assets/cloudformation.template
 
-stack_status=$(aws cloudformation describe-stacks --stack-name ${stack_name} | jq '.Stacks[].StackStatus')
-desired_status="\"CREATE_COMPLETE\""
-timeout=0
-while [ "$desired_status" != "$stack_status" ] && [ "$timeout" -lt 300 ]
-do
-  stack_status=$(aws cloudformation describe-stacks --stack-name ${stack_name} | jq '.Stacks[].StackStatus')
-  echo "waiting for StackStatus to == \"CREATE_COMPLETE\""
-  sleep 20s
-  timeout=$((timeout + 20))
+stack_info=$(aws cloudformation describe-stacks --stack-name ${stack_name})
+stack_status=$(echo $stack_info | jq '.Stacks[].StackStatus')
+while true; do
+  echo "StackStatus ${stack_status}"
+  if [ $stack_status == '"CREATE_IN_PROGRESS"' ]; then
+  echo "sleeping 5"
+  sleep 5s
+  stack_info=$(aws cloudformation describe-stacks --stack-name ${stack_name})
+  stack_status=$(echo $stack_info | jq '.Stacks[].StackStatus')
 done
 
-aws cloudformation describe-stacks --stack-name ${stack_name} > cloudformation_state.json
-
-# exports values into an exports file
-echo -e "#!/usr/bin/env bash" >> $export_file
-echo -e "export DIRECTOR=$(jq '.Stacks[].Outputs[] | select(.OutputKey=="\($base_os)directorvip").OutputValue' cloudformation_state.json --arg base_os ${base_os})" >> $export_file
-echo -e "export VIP=$(jq '.Stacks[].Outputs[] | select(.OutputKey=="\($base_os)batsvip").OutputValue' cloudformation_state.json --arg base_os ${base_os})" >> $export_file
-echo -e "export SUBNET_ID=$(jq '.Stacks[].Outputs[] | select(.OutputKey=="\($base_os)subnetid").OutputValue' cloudformation_state.json --arg base_os ${base_os}))" >> $export_file
-echo -e "export SECURITY_GROUP_NAME=$(jq '.Stacks[].Outputs[] | select(.OutputKey=="securitygroupname").OutputValue' cloudformation_state.json))" >> $export_file
-echo -e "export AVAILABILITY_ZONE=$(jq '.Stacks[].Outputs[] | select(.OutputKey=="\($base_os)availabilityzone").OutputValue' cloudformation_state.json --arg base_os ${base_os}))" >> $export_file
+if [ $stack_status != '"CREATE_COMPLETE"' ]; then
+  echo "cloudformation failed stack info:\n${stack_info}"
+  exit 1
+fi
