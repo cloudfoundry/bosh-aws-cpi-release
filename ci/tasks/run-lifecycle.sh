22336f54eba9d754b3250d65b0ab6f9146d3ea0b
Scott Weiss <scott.weiss@emc.com>
2015-09-02 12:03:55 -0400

    Lifecycle use aws cloudformation cli to get infrastructure information
    Also:
    Since we want to lifecycle run in the same vpc with bats, we use the same aws
    credentials as bats.
    Move some hard code values from secrets to script
    
    Signed-off-by: Xuebin He <xuebin.he@emc.com>

--- ci/tasks/run-lifecycle.sh
+++ ci/tasks/run-lifecycle.sh
@@ -6,10 +6,21 @@ source bosh-cpi-release/ci/tasks/utils.sh
 
 check_param BOSH_AWS_ACCESS_KEY_ID
 check_param BOSH_AWS_SECRET_ACCESS_KEY
-check_param BOSH_AWS_SUBNET_ID
-check_param BOSH_AWS_SUBNET_ZONE
-check_param BOSH_AWS_LIFECYCLE_MANUAL_IP
+check_param region_name
 check_param BOSH_AWS_DEFAULT_KEY_NAME
+
+BOSH_AWS_LIFECYCLE_MANUAL_IP=10.0.2.9
+BOSH_AWS_DEFAULT_KEY_NAME=bats
+
+export AWS_ACCESS_KEY_ID=${aws_access_key_id}
+export AWS_SECRET_ACCESS_KEY=${aws_secret_access_key}
+export AWS_DEFAULT_REGION=${region_name}
+
+stack_name="aws-cpi-stack"
+stack_info=$(get_stack_info $stack_name)
+
+BOSH_AWS_SUBNET_ID=$(get_stack_info_of "${stack_info}" "lifecyclesubnetid")
+BOSH_AWS_SUBNET_ZONE=$(get_stack_info_of "${stack_info}" "lifecycleavailabilityzone")
 
 export BOSH_CLI_SILENCE_SLOW_LOAD_WARNING=true
 
