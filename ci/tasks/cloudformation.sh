4ad8209c2265279495f908bec91298c4473a3d00
Jonathan Barnes <jbarnes@pivotal.io>
2015-10-01 15:48:52 -0700

    Breaks Bosh integration section out of cloudformation template
    We now have a template that we call 'primary' this manages a stack for
    the BATs & Lifecycle suites.  The other template we call
    'bosh-integration' and it is used to manage the Bosh Integration suite.
    Each template is run from it's own concourse job.  The bosh integration
    suite will use this new stack and can be configured to use a different
    AWS account than the primary stack
    
    Also:
     - removed the keypairname input from the templates as they are no
       longer used anywhere
    
    [#104220528](https://www.pivotaltracker.com/story/show/104220528)
    
    Signed-off-by: Aaron Hurley <ahurley@pivotal.io>

--- ci/tasks/cloudformation.sh
+++ ci/tasks/cloudformation.sh
@@ -7,12 +7,11 @@ source bosh-cpi-release/ci/tasks/utils.sh
 check_param aws_access_key_id
 check_param aws_secret_access_key
 check_param region_name
+check_param stack_name
 
 export AWS_ACCESS_KEY_ID=${aws_access_key_id}
 export AWS_SECRET_ACCESS_KEY=${aws_secret_access_key}
 export AWS_DEFAULT_REGION=${region_name}
-
-stack_name="aws-cpi-stack"
 
 stack_info=$(get_stack_info $stack_name)
 vpc_id=$(get_stack_info_of "$stack_info" "VPCID")
@@ -43,7 +42,7 @@ done
 
 cmd="aws cloudformation create-stack \
     --stack-name      ${stack_name} \
-    --template-body   file:///${PWD}/bosh-cpi-release/ci/assets/cloudformation.template.json \
+    --template-body   file:///${PWD}/bosh-cpi-release/ci/assets/cloudformation-${stack_name}.template.json \
     --capabilities    CAPABILITY_IAM"
 
 echo "Running: ${cmd}"; ${cmd}
