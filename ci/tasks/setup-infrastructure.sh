ec1a7818ee5e6de5485d019d3c3a989823b04492
Jonathan Barnes <jbarnes@pivotal.io>
2015-10-09 11:37:52 -0700

    Simplifies download-from-cn micro-pipeline
    With out using some sort of a locking resource the pipeline will fail if
    downloads take longer than the trigger interval because it will queue up
    a create-infrastructure from the current build, then queue up a teardown-infrastructure from
    the previous build then queue up a download task from the current
    build which won't have an infrastructure to run on.
    
    For now we are moving from an auto-triggerd create-infrastructure to a
    manually triggered recreate-infrastructure which will stick around
    inter-build, and the only thing that triggers hourly is the download
    job
    
    [#104220528](https://www.pivotaltracker.com/story/show/104220528)

--- ci/tasks/setup-infrastructure.sh
+++ ci/tasks/setup-infrastructure.sh
@@ -13,24 +13,6 @@ export AWS_ACCESS_KEY_ID=${aws_access_key_id}
 export AWS_SECRET_ACCESS_KEY=${aws_secret_access_key}
 export AWS_DEFAULT_REGION=${region_name}
 
-cmd="aws cloudformation delete-stack --stack-name ${stack_name}"
-echo "Running: ${cmd}"; ${cmd}
-
-while true; do
-  stack_status=$(get_stack_status $stack_name)
-  echo "StackStatus ${stack_status}"
-  if [[ -z "$stack_status" ]]; then #get empty status due to stack not existed on aws
-    echo "No stack found"; break
-    break
-  elif [ $stack_status == 'DELETE_IN_PROGRESS' ]; then
-    echo "${stack_status}: sleeping 5s"; sleep 5s
-  else
-    echo "Expecting the stack to either be deleted or in the process of being deleted but was ${stack_status}"
-    echo $(get_stack_info $stack_name)
-    exit 1
-  fi
-done
-
 cmd="aws cloudformation create-stack \
     --stack-name      ${stack_name} \
     --template-body   file:///${PWD}/bosh-cpi-release/ci/assets/cloudformation-${stack_name}.template.json \
