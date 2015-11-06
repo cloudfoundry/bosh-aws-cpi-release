3e548a01b044b6be03277463ee6285f535658f43
Beyhan Veli <beyhan.veli@sap.com>
2015-11-05 17:57:33 -0800

    Fixes to stemcell-builder pipeline
    Misc fixes to get the light-stemcell-builder job to work including:
     - Remove pipeline configuration variable for region_name since the
       pipeline is about the china region
     - Configures Vagrantfile via a vagrant config file rather than a bunch
       of environment varaibles to help debug issues and be able to re-run
       using the same configuration the build attempted
     - Adds Docker image for china light stemcell since the cpi image didn't
       have vagrant and was inappropriate to use for this build
     - Changes from scp stemcell from concourse worker into china vm to
       wgetting the stemcell from the bosh.io cdn in the hopes that it will
       be faster/more reliable
    
    [#104220500](https://www.pivotaltracker.com/story/show/104220500)
    
    Signed-off-by: Jonathan Barnes <jbarnes@pivotal.io>

--- ci/tasks/build_light_stemcell.sh
+++ ci/tasks/build_light_stemcell.sh
@@ -4,49 +4,60 @@ set -e
 
 source bosh-cpi-release/ci/tasks/utils.sh
 
-
 check_param aws_access_key_id
 check_param aws_secret_access_key
+check_param region_name
 check_param vm_ami
 check_param vm_user
 check_param private_key_data
-check_param vm_vm_name
+check_param public_key_name
+check_param vm_name
 check_param stack_name
-
-
-source /etc/profile.d/chruby.sh
-chruby 2.1.2
-
-stack_info=$(get_stack_info $stack_name)
 
 export AWS_ACCESS_KEY_ID=${aws_access_key_id}
 export AWS_SECRET_ACCESS_KEY=${aws_secret_access_key}
-export VM_AMI=${vm_ami}
-export VM_USER=${vm_user}
-export VM_VM_NAME=${vm_vm_name}
-export BOSH_SRC_PATH=${PWD}/${bosh-src}
-export AWS_SECURITY_GROUP=$(get_stack_info_of "${stack_info}" "SecurityGroupID")
-export VM_KEYPAIR_NAME=$(get_stack_info_of "${stack_info}" "VMKeyPairName")
-export AWS_SUBNET_ID=$(get_stack_info_of "${stack_info}" "SubnetID")
-export AWS_REGION=$(get_stack_info_of "${stack_info}" "RegionName")
-export AWS_ENDPOINT=$(get_stack_info_of "${stack_info}" "RegionEndpoint")
+export AWS_DEFAULT_REGION=${region_name}
+stack_info=$(get_stack_info $stack_name)
 
+private_key_file=$PWD/vm_private_key.pem
+echo "${private_key_data}" > ${private_key_file}
+chmod go-r ${private_key_file}
 
-private_key=$PWD/vm_private_key.pem
-echo "${private_key_data}" > ${private_key}
-chmod go-r ${private_key}
-eval $(ssh-agent)
-ssh-add ${private_key}
+export VAGRANT_CONFIG_FILE="${PWD}/vagrant_light_stemcell_builder_config.json"
+cat > "${VAGRANT_CONFIG_FILE}"<<EOF
+{
+  "AWS_ACCESS_KEY_ID": "${aws_access_key_id}",
+  "AWS_SECRET_ACCESS_KEY": "${aws_secret_access_key}",
+  "AWS_DEFAULT_REGION": "${region_name}",
+  "VM_AMI": "${vm_ami}",
+  "VM_USER": "${vm_user}",
+  "VM_NAME": "${vm_name}",
+  "BOSH_SRC_PATH": "${PWD}/bosh-src",
+  "AWS_SECURITY_GROUP": "$(get_stack_info_of "${stack_info}" "SecurityGroupID")",
+  "VM_KEYPAIR_NAME": "${public_key_name}",
+  "AWS_SUBNET_ID": "$(get_stack_info_of "${stack_info}" "SubnetID")",
+  "AWS_ENDPOINT": "https://$(aws ec2 describe-regions | jq -r --arg key ${region_name} '.Regions[] | select(.RegionName=="\($key)").Endpoint')",
+  "VM_PRIVATE_KEY_FILE": "${private_key_file}"
+}
+EOF
 
 workspace="${PWD}/bosh-cpi-release/ci/light_stemcell_builder"
 out_dir="${PWD}/out"
-full_stemcell_name=china-$(cat bosh-aws-full-stemcell/url | grep -o "[^/]*$")
+
+full_stemcell_file=${PWD}/bosh-aws-full-stemcell/stemcell.tgz
+full_stemcell_url=$(cat bosh-aws-full-stemcell/url | awk '{ gsub(/light-/, ""); print }')
+full_stemcell_name=china-$(echo ${full_stemcell_url} | grep -o "[^/]*$")
 
 pushd ${workspace}
+  vagrant box add dummy https://github.com/mitchellh/vagrant-aws/raw/master/dummy.box
   vagrant up --provider=aws
-  vagrant ssh-config > ./vagrant.ssh.config
-  scp -F vagrant.ssh.config bosh-aws-full-stemcell/stemcell.tgz default:~/${full_stemcell_name}
-  vagrant ssh -c "cd /bosh/bosh-stemcell && bundle exec rake stemcell:build_light[~/${full_stemcell_name},hvm]"
+  vagrant ssh << EOF
+    wget ${full_stemcell_url} /home/${vm_user}/${full_stemcell_name}
+    cd /bosh/bosh-stemcell
+    bundle exec rake stemcell:build_light[/home/${vm_user}/${full_stemcell_name},hvm]
+EOF
   mkdir -p ${out_dir}
-  scp -F vagrant.ssh.config default:~/*light*.tgz ${out_dir}
+  vagrant ssh-config > ./vagrant.ssh.config
+  scp -F vagrant.ssh.config default:/home/${vm_user}/*light*.tgz ${out_dir}
+  vagrant destroy -f
 popd
