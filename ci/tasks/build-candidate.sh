2768729208ae1ca654e4af77a0a17196cc37283b
Jonathan Barnes <jbarnes@pivotal.io>
2015-11-17 14:22:05 -0800

    the build-candidate explicitly outputs `candidate`
    Steps run after this task now expect to have access to a `candidate` input
    
    [#108268384](https://www.pivotaltracker.com/story/show/108268384)
    
    Signed-off-by: Edwin Xie <exie@pivotal.io>

--- ci/tasks/build-candidate.sh
+++ ci/tasks/build-candidate.sh
@@ -7,22 +7,20 @@ chruby 2.1.2
 
 semver=`cat version-semver/number`
 
-mkdir out
+pushd bosh-cpi-src
+  echo "running unit tests"
+  pushd src/bosh_aws_cpi
+    bundle install
+    bundle exec rspec spec/unit/*
+  popd
 
-cd bosh-cpi-src
+  echo "using bosh CLI version..."
+  bosh version
 
-echo "running unit tests"
-pushd src/bosh_aws_cpi
-  bundle install
-  bundle exec rspec spec/unit/*
+  cpi_release_name="bosh-aws-cpi"
+
+  echo "building CPI release..."
+  bosh create release --name $cpi_release_name --version $semver --with-tarball
 popd
 
-echo "using bosh CLI version..."
-bosh version
-
-cpi_release_name="bosh-aws-cpi"
-
-echo "building CPI release..."
-bosh create release --name $cpi_release_name --version $semver --with-tarball
-
-mv dev_releases/$cpi_release_name/$cpi_release_name-$semver.tgz ../out/
+mv bosh-cpi-src/dev_releases/$cpi_release_name/$cpi_release_name-$semver.tgz candidate/
