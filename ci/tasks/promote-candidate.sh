ad6ca521ba8c3275f405d57f8c0791f46d97988a
Edwin Xie <exie@pivotal.io>
2015-11-17 17:16:35 -0800

    promote-candidate task declares explicit outputs
    Since we have a later step that is dependent on changes from this task
    we made an explicit output called promoted that contains the git repo
    with the new commit and an integer_version file to tag the commit when
    pushing it.
    
    [#108268384](https://www.pivotaltracker.com/story/show/108268384)
    
    Signed-off-by: Jonathan Barnes <jbarnes@pivotal.io>

--- ci/tasks/promote-candidate.sh
+++ ci/tasks/promote-candidate.sh
@@ -4,38 +4,42 @@ set -e -x
 
 source bosh-cpi-src/ci/tasks/utils.sh
 
-# Creates an integer version number from the semantic version format
-# May be changed when we decide to fully use semantic versions for releases
-integer_version=`cut -d "." -f1 release-version-semver/number`
-echo $integer_version > integer_version
-
-cd bosh-cpi-src
-
 source /etc/profile.d/chruby.sh
 chruby 2.1.2
 
-set +x
-echo creating config/private.yml with blobstore secrets
-cat > config/private.yml << EOF
+# Creates an integer version number from the semantic version format
+# May be changed when we decide to fully use semantic versions for releases
+integer_version=`cut -d "." -f1 release-version-semver/number`
+echo $integer_version > promoted/integer_version
+
+cp -r bosh-cpi-src promoted/repo
+
+dev_release=$(echo $PWD/bosh-cpi-release/*.tgz)
+
+pushd promoted/repo
+  set +x
+  echo creating config/private.yml with blobstore secrets
+  cat > config/private.yml << EOF
 ---
 blobstore:
   s3:
     access_key_id: $aws_access_key_id
     secret_access_key: $aws_secret_access_key
 EOF
-set -x
+  set -x
 
-echo "using bosh CLI version..."
-bosh version
+  echo "using bosh CLI version..."
+  bosh version
 
-echo "finalizing CPI release..."
-bosh finalize release ../bosh-cpi-release/*.tgz --version $integer_version
+  echo "finalizing CPI release..."
+  bosh finalize release ${dev_release} --version $integer_version
 
-rm config/private.yml
+  rm config/private.yml
 
-git diff | cat
-git add .
+  git diff | cat
+  git add .
 
-git config --global user.email cf-bosh-eng@pivotal.io
-git config --global user.name CI
-git commit -m "New final release v $integer_version"
+  git config --global user.email cf-bosh-eng@pivotal.io
+  git config --global user.name CI
+  git commit -m "New final release v $integer_version"
+popd
