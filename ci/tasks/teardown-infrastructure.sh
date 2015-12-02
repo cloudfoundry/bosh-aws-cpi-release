bd6b95845d70c12f95d42fbf0fc6031b8f5755b7
Aaron Hurley <ahurley@pivotal.io>
2015-12-02 08:22:00 -0800

    Empties a bucket's contents before attempting to delete the stack
    In the teardown-infrastructure task, the Cloudformation command of deleting a stack will fail if a bucket exists and is not empty. Now we will check to see if a bucket exists and if it has contents, if so, it will be emptied before attempting to delete the Cloudformation stack. The End2End tests require a 'stack_prefix' param while the others do not (since a bucket is not needed for those).
    
    [#109367456](https://www.pivotaltracker.com/story/show/109367456)

--- ci/tasks/teardown-infrastructure.sh
+++ ci/tasks/teardown-infrastructure.sh
@@ -8,10 +8,17 @@ check_param aws_access_key_id
 check_param aws_secret_access_key
 check_param region_name
 check_param stack_name
+check_param stack_prefix
 
 export AWS_ACCESS_KEY_ID=${aws_access_key_id}
 export AWS_SECRET_ACCESS_KEY=${aws_secret_access_key}
 export AWS_DEFAULT_REGION=${region_name}
+
+stack_info=$(get_stack_info $stack_name)
+bucket_name=$(get_stack_info_of "${stack_info}" "${stack_prefix}BlobstoreBucketName")
+if [[ ! -z "${bucket_name}" ]] && (aws s3 ls s3://${bucket_name}) ; then
+  aws s3 rm s3://${bucket_name} --recursive
+fi
 
 cmd="aws cloudformation delete-stack --stack-name ${stack_name}"
 echo "Running: ${cmd}"; ${cmd}
