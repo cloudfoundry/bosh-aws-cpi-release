f3bd192357b0ddd729bf662edebfd3b5e6de3e91
Lyle Franklin <lfranklin@pivotal.io>
2016-04-01 14:25:34 -0700

    Add integration tests for custom endpoints and CA bundles
    - Tests that CPI can contact AWS using a specified endpoint rather than a
      region
    - Tests that the CPI will verify the endpoints SSL certificate if
      configured with a CA bundle
    
    [#116481439](https://www.pivotaltracker.com/story/show/116481439)
    [#116318655](https://www.pivotaltracker.com/story/show/116318655)
    
    Signed-off-by: Caleb Miles <cmiles@pivotal.io>

---
+++ src/bosh_aws_cpi/spec/integration/endpoint_spec.rb
@@ -0,0 +1,90 @@
+require 'spec_helper'
+require 'logger'
+require 'cloud'
+require 'open-uri'
+
+describe Bosh::AwsCloud::Cloud do
+  before(:all) do
+    @access_key_id     = ENV['BOSH_AWS_ACCESS_KEY_ID']       || raise("Missing BOSH_AWS_ACCESS_KEY_ID")
+    @secret_access_key = ENV['BOSH_AWS_SECRET_ACCESS_KEY']   || raise("Missing BOSH_AWS_SECRET_ACCESS_KEY")
+  end
+
+  before { allow(Bosh::Clouds::Config).to receive_messages(logger: logger) }
+  let(:logger) { Logger.new(STDERR) }
+
+  describe 'specifying ec2 endpoint instead of region' do
+    let(:cpi) do
+      Bosh::AwsCloud::Cloud.new(
+        'aws' => {
+          'ec2_endpoint' => 'https://ec2.sa-east-1.amazonaws.com',
+          'elb_endpoint' => 'https://elb.sa-east-1.amazonaws.com',
+          'access_key_id' => @access_key_id,
+          'default_key_name' => 'fake-key',
+          'secret_access_key' => @secret_access_key
+        },
+        'registry' => {
+          'endpoint' => 'fake',
+          'user' => 'fake',
+          'password' => 'fake'
+        }
+      )
+    end
+
+    it 'uses the given endpoint' do
+      expect {
+        cpi.has_vm?('i-nope')
+      }.to_not raise_error
+    end
+  end
+
+  describe 'using a custom CA bundle' do
+    let(:cpi) do
+      Bosh::AwsCloud::Cloud.new(
+        'aws' => {
+          'region' => 'us-east-1',
+          'default_key_name' => 'fake-key',
+          'access_key_id' => @access_key_id,
+          'secret_access_key' => @secret_access_key
+        },
+        'registry' => {
+          'endpoint' => 'fake',
+          'user' => 'fake',
+          'password' => 'fake'
+        }
+      )
+    end
+
+
+    before(:each) { ENV.delete('BOSH_CA_CERT_FILE') }
+
+    context 'when the certificate returned from the server contains a CA in the provided bundle' do
+      it 'completes requests over SSL' do
+        begin
+          valid_bundle = File.open('valid-ca-bundle', 'w+') do |f|
+            # Download the CA bundle that is included in the AWS SDK
+            f << open('https://raw.githubusercontent.com/aws/aws-sdk-ruby/master/aws-sdk-core/ca-bundle.crt').read
+          end
+
+          ENV['BOSH_CA_CERT_FILE'] = valid_bundle.path
+
+          expect {
+            cpi.has_vm?('i-nope')
+          }.to_not raise_error
+        ensure
+          File.delete(valid_bundle.path)
+        end
+
+      end
+    end
+
+    context 'when the certificate returned from the server does not contain a CA in the provided bundle' do
+      it 'raises an SSL verification error' do
+        ENV['BOSH_CA_CERT_FILE'] = asset('invalid-cert.pem')
+
+        expect {
+          cpi.has_vm?('i-nope')
+        }.to raise_error(OpenSSL::SSL::SSLError)
+      end
+    end
+  end
+end
