f035d42b4f6c4796f778349d80ceeed115c3f319
Lyle Franklin <lfranklin@pivotal.io>
2016-07-15 10:13:25 -0700

    Include x1 instance type in calculate_vm_cloud_properties
    [#126023265](https://www.pivotaltracker.com/story/show/126023265)

--- src/bosh_aws_cpi/lib/cloud/aws/instance_type_mapper.rb
+++ src/bosh_aws_cpi/lib/cloud/aws/instance_type_mapper.rb
@@ -2,21 +2,22 @@ module Bosh::AwsCloud
   class InstanceTypeMapper
 
     SUPPORTED_VM_TYPES = [
-      {name: 't2.nano',    cpu: 1,  ram: 0.5 * 1024},
-      {name: 't2.micro',   cpu: 1,  ram: 1 * 1024},
-      {name: 't2.small',   cpu: 1,  ram: 2 * 1024},
-      {name: 'c4.large',   cpu: 2,  ram: 3.75 * 1024},
-      {name: 'm4.large',   cpu: 2,  ram: 8 * 1024},
-      {name: 'r3.large',   cpu: 2,  ram: 15.25 * 1024},
-      {name: 'c4.xlarge',  cpu: 4,  ram: 7.5 * 1024},
-      {name: 'm4.xlarge',  cpu: 4,  ram: 16 * 1024},
-      {name: 'r3.xlarge',  cpu: 4,  ram: 30.5 * 1024},
-      {name: 'c4.2xlarge', cpu: 8,  ram: 15 * 1024},
-      {name: 'm4.2xlarge', cpu: 8,  ram: 32 * 1024},
-      {name: 'r3.2xlarge', cpu: 8,  ram: 61 * 1024},
-      {name: 'c4.4xlarge', cpu: 16, ram: 30 * 1024},
-      {name: 'm4.4xlarge', cpu: 16, ram: 64 * 1024},
-      {name: 'r3.4xlarge', cpu: 16, ram: 122 * 1024},
+      {name: 't2.nano',     cpu: 1,   ram: 0.5 * 1024},
+      {name: 't2.micro',    cpu: 1,   ram: 1 * 1024},
+      {name: 't2.small',    cpu: 1,   ram: 2 * 1024},
+      {name: 'c4.large',    cpu: 2,   ram: 3.75 * 1024},
+      {name: 'm4.large',    cpu: 2,   ram: 8 * 1024},
+      {name: 'r3.large',    cpu: 2,   ram: 15.25 * 1024},
+      {name: 'c4.xlarge',   cpu: 4,   ram: 7.5 * 1024},
+      {name: 'm4.xlarge',   cpu: 4,   ram: 16 * 1024},
+      {name: 'r3.xlarge',   cpu: 4,   ram: 30.5 * 1024},
+      {name: 'c4.2xlarge',  cpu: 8,   ram: 15 * 1024},
+      {name: 'm4.2xlarge',  cpu: 8,   ram: 32 * 1024},
+      {name: 'r3.2xlarge',  cpu: 8,   ram: 61 * 1024},
+      {name: 'c4.4xlarge',  cpu: 16,  ram: 30 * 1024},
+      {name: 'm4.4xlarge',  cpu: 16,  ram: 64 * 1024},
+      {name: 'r3.4xlarge',  cpu: 16,  ram: 122 * 1024},
+      {name: 'x1.32xlarge', cpu: 128, ram: 1952 * 1024},
     ]
 
     def initialize
