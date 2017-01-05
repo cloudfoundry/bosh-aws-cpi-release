b726259679bd929136ff0d7f9b8469a6e8d84422
Zak Auerbach <zauerbach@pivotal.io>
2017-01-05 15:33:50 -0800

    Fix delete_snapshot_spec
    Signed-off-by: Lyle Franklin <lfranklin@pivotal.io>

--- src/bosh_aws_cpi/spec/unit/delete_snapshot_spec.rb
+++ src/bosh_aws_cpi/spec/unit/delete_snapshot_spec.rb
@@ -6,25 +6,14 @@ describe Bosh::AwsCloud::Cloud do
 
     let(:cloud) {
       mock_cloud do |ec2|
-        snapshots = double(Aws::EC2::SnapshotCollection, :[] => snapshot)
-        allow(ec2).to receive_messages(snapshots: snapshots)
+        allow(ec2).to receive(:snapshot).with('snap-xxxxxxxx').and_return(snapshot)
       end
     }
 
     it 'should delete a snapshot' do
-      expect(snapshot).to receive(:status).and_return(:available)
       expect(snapshot).to receive(:delete)
 
       cloud.delete_snapshot('snap-xxxxxxxx')
-    end
-
-    it 'should raise an error if the snapshot is in use' do
-      expect(snapshot).to receive(:status).and_return(:in_use)
-      expect(snapshot).not_to receive(:delete)
-
-      expect {
-        cloud.delete_snapshot('snap-xxxxxxxx')
-      }.to raise_error Bosh::Clouds::CloudError, %q{snapshot 'snap-xxxxxxxx' can not be deleted as it is in use}
     end
   end
 end
