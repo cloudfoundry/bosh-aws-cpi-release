104cec4c4105a0f381a8b78f5e5df7ebe9de77be
Zak Auerbach <zauerbach@pivotal.io>
2017-01-05 13:42:35 -0800

    Fix cloud.rb unit tests
    Signed-off-by: Lyle Franklin <lfranklin@pivotal.io>

--- src/bosh_aws_cpi/spec/unit/cloud_spec.rb
+++ src/bosh_aws_cpi/spec/unit/cloud_spec.rb
@@ -9,8 +9,7 @@ describe Bosh::AwsCloud::Cloud do
 
   before do
     allow(Bosh::AwsCloud::AvailabilityZoneSelector).to receive(:new).and_return(az_selector)
-    reg = Aws::EC2::Region.new('fake-region', endpoint: 'http://some.endpoint')
-    allow_any_instance_of(Aws::EC2).to receive(:regions).and_return([reg])
+    allow_any_instance_of(Aws::EC2::Resource).to receive(:subnets).and_return([double('subnet')])
   end
 
   describe '#initialize' do
@@ -54,14 +53,6 @@ describe Bosh::AwsCloud::Cloud do
         end
       end
 
-      context 'when optional properties are not provided' do
-        it 'default value is used for http properties' do
-          expect(cloud.ec2_client.config.http_read_timeout).to eq(60)
-          expect(cloud.ec2_client.config.http_wire_trace).to be false
-          expect(cloud.ec2_client.config.ssl_verify_peer).to be true
-        end
-      end
-
       context 'when optional and required properties are provided' do
         let(:options) do
           mock_cloud_properties_merge(
@@ -74,26 +65,8 @@ describe Bosh::AwsCloud::Cloud do
         end
 
         it 'passes required properties to AWS SDK' do
-          config = cloud.ec2_client.config
+          config = cloud.ec2_resource.client.config
           expect(config.region).to eq('fake-region')
-        end
-      end
-
-      context 'when the given region is invalid' do
-        let(:regions) { instance_double(Aws::EC2::RegionCollection) }
-        before do
-          allow_any_instance_of(Aws::EC2).to receive(:regions).and_return(regions)
-        end
-
-        it 'raises a cloud error' do
-          allow(regions).to receive(:first).and_raise(
-            SocketError,
-            'getaddrinfo: Name or service not known',
-          )
-          expect { cloud }.to raise_error(
-            Bosh::Clouds::CloudError,
-            /region.*getaddrinfo/m,
-          )
         end
       end
     end
@@ -109,20 +82,15 @@ describe Bosh::AwsCloud::Cloud do
     end
 
     before do
-      allow(Bosh::AwsCloud::ResourceWait).to receive(:for_volume).with(volume: volume, state: :available)
+      allow(Bosh::AwsCloud::ResourceWait).to receive(:for_volume).with(volume: volume, state: 'available')
     end
 
     context 'when volumes are set' do
-      let(:ec2_client) { instance_double('Aws::EC2', client: low_level_client) }
-      let(:low_level_client) { instance_double('Aws::EC2::Client::V20141001') }
-      let(:volume_resp) { double('Aws::Core::Response', volume_id: 'fake-volume-id') }
-      let(:volume) { double('Aws::EC2::Volume', id: 'fake-volume-id') }
+      let(:ec2_client) { instance_double(Aws::EC2::Client) }
+      let(:volume_resp) { instance_double(Aws::EC2::Types::Volume, volume_id: 'fake-volume-id') }
+      let(:volume) { instance_double(Aws::EC2::Volume, id: 'fake-volume-id') }
       before do
-        cloud.instance_variable_set(:'@ec2_client', ec2_client)
-        allow(ec2_client).to receive(:config).and_return('fake-config')
-        allow(Aws::EC2::Volume).to receive(:new_from)
-          .with(:create_volume, volume_resp, 'fake-volume-id', config: 'fake-config')
-          .and_return(volume)
+        cloud.instance_variable_set(:@ec2_client, ec2_client)
       end
 
       context 'when disk type is provided' do
@@ -135,12 +103,17 @@ describe Bosh::AwsCloud::Cloud do
             let(:disk_type) { 'gp2' }
 
             it 'creates disk with gp2 type' do
-              expect(low_level_client).to receive(:create_volume).with(
+              expect(ec2_client).to receive(:create_volume).with(
                 size: 10000,
                 availability_zone: 'fake-availability-zone',
                 volume_type: 'gp2',
                 encrypted: false
               ).and_return(volume_resp)
+
+              allow(Aws::EC2::Volume).to receive(:new).with({
+                id: 'fake-volume-id',
+                client: ec2_client,
+              }).and_return(volume)
               cloud.create_disk(disk_size, cloud_properties, 42)
             end
           end
@@ -150,13 +123,19 @@ describe Bosh::AwsCloud::Cloud do
             let(:disk_type) { 'io1' }
 
             it 'creates disk with io1 type' do
-              expect(low_level_client).to receive(:create_volume).with(
+              expect(ec2_client).to receive(:create_volume).with(
                 size: 10000,
                 availability_zone: 'fake-availability-zone',
                 volume_type: 'io1',
                 iops: 123,
                 encrypted: false
               ).and_return(volume_resp)
+
+              allow(Aws::EC2::Volume).to receive(:new).with({
+                id: 'fake-volume-id',
+                client: ec2_client,
+              }).and_return(volume)
+
               cloud.create_disk(disk_size, cloud_properties, 42)
             end
           end
@@ -169,12 +148,18 @@ describe Bosh::AwsCloud::Cloud do
             let(:disk_type) { 'standard' }
 
             it 'creates disk with the specified type' do
-              expect(low_level_client).to receive(:create_volume).with(
+              expect(ec2_client).to receive(:create_volume).with(
                 size: 2,
                 availability_zone: 'fake-availability-zone',
                 volume_type: 'standard',
                 encrypted: false
               ).and_return(volume_resp)
+
+              allow(Aws::EC2::Volume).to receive(:new).with({
+                id: 'fake-volume-id',
+                client: ec2_client,
+              }).and_return(volume)
+
               cloud.create_disk(disk_size, cloud_properties, 42)
             end
           end
@@ -186,12 +171,18 @@ describe Bosh::AwsCloud::Cloud do
         let(:disk_size) { 1025 }
 
         it 'creates disk with gp2 disk type' do
-          expect(low_level_client).to receive(:create_volume).with(
+          expect(ec2_client).to receive(:create_volume).with(
             size: 2,
             availability_zone: 'fake-availability-zone',
             volume_type: 'gp2',
             encrypted: false
           ).and_return(volume_resp)
+
+          allow(Aws::EC2::Volume).to receive(:new).with({
+            id: 'fake-volume-id',
+            client: ec2_client,
+          }).and_return(volume)
+
           cloud.create_disk(disk_size, cloud_properties, 42)
         end
       end
@@ -234,6 +225,11 @@ describe Bosh::AwsCloud::Cloud do
             }
         )
       end
+
+      before(:each) do
+        allow(Aws::InstanceProfileCredentials).to receive(:new).and_return(double(Aws::InstanceProfileCredentials))
+      end
+
       it 'does not raise an error ' do
         expect { cloud }.to_not raise_error
       end
