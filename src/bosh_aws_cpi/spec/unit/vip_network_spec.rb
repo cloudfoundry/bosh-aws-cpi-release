4144761113a8c9bb773d5beeb3fa46c45d66b4eb
Lyle Franklin <lfranklin@pivotal.io>
2017-01-06 14:53:36 -0800

    Fix VIP networking
    Signed-off-by: Chris Dutra <cdutra@pivotal.io>

--- src/bosh_aws_cpi/spec/unit/vip_network_spec.rb
+++ src/bosh_aws_cpi/spec/unit/vip_network_spec.rb
@@ -1,29 +1,34 @@
 require 'spec_helper'
 
 describe Bosh::AwsCloud::VipNetwork do
-  let(:ec2) { double(Aws::EC2) }
-  let(:instance) { double(Aws::EC2::Instance, :id => 'id') }
+  let(:ec2_resource) { instance_double(Aws::EC2::Resource, client: ec2_client) }
+  let(:ec2_client) { instance_double(Aws::EC2::Client) }
+  let(:instance) { instance_double(Aws::EC2::Instance, :id => 'fake-id') }
+
+  before(:each) do
+    allow(Kernel).to receive(:sleep)
+  end
 
   it 'should require an IP' do
     vip = described_class.new('vip', {})
     expect {
-      vip.configure(ec2, instance)
+      vip.configure(ec2_resource, instance)
     }.to raise_error Bosh::Clouds::CloudError
   end
 
-  [Aws::EC2::Errors::IncorrectInstanceState, Aws::EC2::Errors::InvalidInstanceID].each do |error|
+  [Aws::EC2::Errors::IncorrectInstanceState.new(nil, 'bad-state'), Aws::EC2::Errors::InvalidInstanceID.new(nil, 'bad-id')].each do |error|
     context "when AWS returns an #{error} error" do
       it 'should retry to attach until it succeeds' do
         vip = described_class.new('vip', {'ip' => '1.2.3.4'})
 
-        elastic_ip = double('eip')
-        allow(ec2).to receive_message_chain(:elastic_ips, :[]).and_return(elastic_ip)
-        allow(Bosh::Common).to receive(:sleep)
+        expect(ec2_client).to receive(:associate_address)
+          .with(instance_id: 'fake-id', public_ip: '1.2.3.4')
+          .and_raise(error)
+        expect(ec2_client).to receive(:associate_address)
+          .with(instance_id: 'fake-id', public_ip: '1.2.3.4')
+          .and_return(true)
 
-        expect(instance).to receive(:associate_elastic_ip).and_raise(error)
-        expect(instance).to receive(:associate_elastic_ip).with(elastic_ip).and_return(true)
-
-        vip.configure(ec2, instance)
+        vip.configure(ec2_resource, instance)
       end
     end
   end
