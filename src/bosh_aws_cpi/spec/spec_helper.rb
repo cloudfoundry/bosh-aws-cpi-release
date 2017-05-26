d90cc5d67caf9ec14fbc76af47b94d7a336432d9
Chris Dutra <cdutra@pivotal.io>
2017-05-26 12:44:40 -0700

    Add default PROJECT_RUBY_VERSION (2.1.2)
    [#146126329](https://www.pivotaltracker.com/story/show/146126329)

--- src/bosh_aws_cpi/spec/spec_helper.rb
+++ src/bosh_aws_cpi/spec/spec_helper.rb
@@ -6,7 +6,7 @@ require 'cloud/aws'
 
 MOCK_AWS_ACCESS_KEY_ID = 'foo'
 MOCK_AWS_SECRET_ACCESS_KEY = 'bar'
-PROJECT_RUBY_VERSION = ENV['PROJECT_RUBY_VERSION']
+PROJECT_RUBY_VERSION = ENV.fetch('PROJECT_RUBY_VERSION', '2.1.2')
 
 def mock_cloud_options
   {
