0c012077af871b1dd360a6e6a8c6d879e2791c38
Chris Dutra <cdutra@pivotal.io>
2017-07-03 18:45:23 -0700

    Move end-2-end release into aws cpi repo
    motivation:
    - it contains bosh-errands with tests specific for AWS which are not
    covered in the lifecycle tests nor BATs
    
    [#147049339](https://www.pivotaltracker.com/story/show/147049339)
    
    Signed-off-by: Shoabe Shariff <sshariff@pivotal.io>

--- ci/tasks/run-e2e.sh
+++ ci/tasks/run-e2e.sh
@@ -8,11 +8,13 @@ cp "${bosh_cli}" /usr/local/bin/bosh2
 
 source director-state/director.env
 
-pushd pipelines/aws/assets/e2e-test-release
+# CREATE TEST RELEASE
+pushd bosh-cpi-src/ci/assets/e2e-test-release
   time bosh2 -n create-release --force --name e2e-test --version 1.0.0
   time bosh2 -n upload-release
 popd
 
+# UPLOAD STEMCELL
 time bosh2 -n upload-stemcell "$(realpath stemcell/*.tgz)"
 time bosh2 -n upload-stemcell "$(realpath heavy-stemcell/*.tgz)"
 
@@ -29,17 +31,20 @@ time bosh2 repack-stemcell \
 time bosh2 -n upload-stemcell /tmp/e2e-encrypted-heavy-stemcell.tgz
 encrypted_heavy_stemcell_ami_id="$( bosh2 stemcells | grep e2e-encrypted-heavy-stemcell | awk '{print $NF;}' )"
 
+# UPDATE CLOUD CONFIG
 time bosh2 -n ucc \
   -l environment/metadata \
-  pipelines/aws/assets/e2e-test-release/cloud-config-2.yml
+  bosh-cpi-src/ci/assets/e2e-test-release/cloud-config.yml
 
+# BOSH DEPLOY
 time bosh2 -n deploy -d e2e-test \
   -v "stemcell_name=${stemcell_name}" \
   -v "heavy_stemcell_name=${heavy_stemcell_name}" \
   -v "encrypted_heavy_stemcell_ami_id=${encrypted_heavy_stemcell_ami_id}" \
   -l environment/metadata \
-  pipelines/aws/assets/e2e-test-release/manifest.yml
+  bosh-cpi-src/ci/assets/e2e-test-release/manifest.yml
 
+# RUN ERRANDS
 time bosh2 -n run-errand -d e2e-test iam-instance-profile-test
 time bosh2 -n run-errand -d e2e-test raw-ephemeral-disk-test
 time bosh2 -n run-errand -d e2e-test elb-registration-test
