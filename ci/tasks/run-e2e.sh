0b2939fa94c226617820e48ec865aa4caf9d90b1
Chris Dutra <cdutra@pivotal.io>
2017-09-14 11:08:26 -0700

    use ubuntu:14.04, verify shasum and pgp signatures
    [#148919557](https://www.pivotaltracker.com/story/show/148919557)
    
    Signed-off-by: Neha Jain <jneha@vmware.com>

--- ci/tasks/run-e2e.sh
+++ ci/tasks/run-e2e.sh
@@ -42,11 +42,11 @@ time bosh2 -n deploy -d e2e-test \
   bosh-cpi-src/ci/assets/e2e-test-release/manifest.yml
 
 # RUN ERRANDS
-time bosh2 -n run-errand -d e2e-test iam-instance-profile-test & \
-  bosh2 -n run-errand -d e2e-test raw-ephemeral-disk-test & \
-  bosh2 -n run-errand -d e2e-test elb-registration-test & \
-  bosh2 -n run-errand -d e2e-test heavy-stemcell-test & \
-  bosh2 -n run-errand -d e2e-test encrypted-heavy-stemcell-test
+time bosh2 -n run-errand -d e2e-test iam-instance-profile-test
+time bosh2 -n run-errand -d e2e-test raw-ephemeral-disk-test
+time bosh2 -n run-errand -d e2e-test elb-registration-test
+time bosh2 -n run-errand -d e2e-test heavy-stemcell-test
+time bosh2 -n run-errand -d e2e-test encrypted-heavy-stemcell-test
 
 # spot instances do not work in China
 region=$( jq -e --raw-output ".region" environment/metadata )
