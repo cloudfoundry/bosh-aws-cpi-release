2616e8916c86bed52190db215fa5eaa6724b28fd
Luan Santos <lsantos@pivotal.io>
2017-12-04 12:00:09 -0800

    Don't expose elb_params nor ec2_params
    [#152249108](https://www.pivotaltracker.com/story/show/152249108)
    
    Signed-off-by: Chris Dutra <cdutra@pivotal.io>

--- src/bosh_aws_cpi/lib/cloud/aws/aws_provider.rb
+++ src/bosh_aws_cpi/lib/cloud/aws/aws_provider.rb
@@ -2,18 +2,15 @@ module Bosh::AwsCloud
   class AwsProvider
     include Helpers
 
-    attr_reader :ec2_client, :ec2_resource, :alb_client, :elb_client, :elb_params, :ec2_params
+    attr_reader :ec2_client, :ec2_resource, :alb_client, :elb_client
 
     def initialize(aws_config, logger)
       @aws_config = aws_config
       @logger = logger
 
       @elb_params = initialize_params(@aws_config.elb_endpoint)
-
       @elb_client = Aws::ElasticLoadBalancing::Client.new(@elb_params)
       @alb_client = Aws::ElasticLoadBalancingV2::Client.new(@elb_params)
-
-      @ec2_params = initialize_params(@aws_config.ec2_endpoint)
 
       # AWS Ruby SDK is threadsafe but Ruby autoload isn't,
       # so we need to trigger eager autoload while constructing CPI
@@ -21,6 +18,7 @@ module Bosh::AwsCloud
 
       # In SDK v2 the default is more request driven, while the old 'model way' lives in Resource.
       # Therefore in most cases Aws::EC2::Resource would replace the client.
+      @ec2_params = initialize_params(@aws_config.ec2_endpoint)
       @ec2_client = Aws::EC2::Client.new(@ec2_params)
       @ec2_resource = Aws::EC2::Resource.new(client: @ec2_client)
     end
--- src/bosh_aws_cpi/spec/unit/aws_provider_spec.rb
+++ src/bosh_aws_cpi/spec/unit/aws_provider_spec.rb
@@ -1,108 +1,77 @@
 require 'spec_helper'
 
-describe Bosh::AwsCloud::AwsProvider do
 
+
+describe Bosh::AwsCloud::AwsProvider do
   let(:options) do
     opts = mock_cloud_options['properties']
     opts
   end
   let(:logger) { Bosh::Clouds::Config.logger}
   let(:config) { Bosh::AwsCloud::Config.build(options.dup.freeze) }
-  let(:provider) { Bosh::AwsCloud::AwsProvider.new(config.aws, logger) }
+  let(:params) do
+    {
+      credentials: config.aws.credentials,
+      retry_limit: 8,
+      logger: logger,
+      log_level: :debug,
+      region: 'us-east-1'
+    }
+  end
+  let(:ec2_client) { instance_double(Aws::EC2::Client) }
 
-  it 'should configure elb_params' do
-    params = provider.elb_params
-    expect(params[:region]).to eq('us-east-1')
-    expect(params[:endpoint]).to eq(nil)
-    expect(params[:ssl_ca_bundle]).to eq(nil)
-    expect(params[:retry_limit]).to eq(8)
-    expect(params[:log_level]).to eq(:debug)
+  def configures_client_with_params
+    expect(Aws::ElasticLoadBalancing::Client).to receive(:new).with(params)
+    expect(Aws::ElasticLoadBalancingV2::Client).to receive(:new).with(params)
+    expect(Aws::EC2::Client).to receive(:new).with(params).and_return(ec2_client)
+    expect(Aws::EC2::Resource).to receive(:new).with(client: ec2_client)
+
+    Bosh::AwsCloud::AwsProvider.new(config.aws, logger)
   end
 
-  it 'should configure ec2_params' do
-    params = provider.ec2_params
-    expect(params[:region]).to eq('us-east-1')
-    expect(params[:endpoint]).to eq(nil)
-    expect(params[:ssl_ca_bundle]).to eq(nil)
-    expect(params[:retry_limit]).to eq(8)
-    expect(params[:logger]).to be_truthy
-    expect(params[:log_level]).to eq(:debug)
-  end
+  it { configures_client_with_params }
 
-  context 'no region is set' do
-    let(:options) do
-      opts = mock_cloud_options['properties']
-      opts['aws'].delete('region')
-      opts['aws'].store("ec2_endpoint", "http://the_ec2_endpoint.com")
-      opts['aws'].store("elb_endpoint", "http://the_elb_endpoint.com")
-      opts
+  context 'with endpoints set' do
+    context 'with scheme' do
+      before do
+        options['aws'].delete('region')
+        options['aws'].store('ec2_endpoint', 'http://the_endpoint.com')
+        options['aws'].store('elb_endpoint', 'http://the_endpoint.com')
+        params[:endpoint] = 'http://the_endpoint.com'
+        params.delete(:region)
+      end
+
+      it { configures_client_with_params }
     end
 
-    it 'should configure elb_params' do
-      params = provider.elb_params
-      expect(params[:region]).to eq(nil)
-    end
+    context 'without scheme' do
+      before do
+        options['aws'].delete('region')
+        options['aws'].store('ec2_endpoint', 'the_endpoint.com')
+        options['aws'].store('elb_endpoint', 'the_endpoint.com')
+        params[:endpoint] = 'https://the_endpoint.com'
+        params.delete(:region)
+      end
 
-    it 'should configure ec2_params' do
-      params = provider.ec2_params
-      expect(params[:region]).to eq(nil)
-    end
-  end
-
-  context 'endpoints are set with protocol' do
-    let(:options) do
-      opts = mock_cloud_options['properties']
-      opts['aws'].store("ec2_endpoint", "http://the_ec2_endpoint.com")
-      opts['aws'].store("elb_endpoint", "http://the_elb_endpoint.com")
-      opts
-    end
-
-    it 'should configure elb_params' do
-      params = provider.elb_params
-      expect(params[:endpoint]).to eq("http://the_elb_endpoint.com")
-    end
-
-    it 'should configure ec2_params' do
-      params = provider.ec2_params
-      expect(params[:endpoint]).to eq("http://the_ec2_endpoint.com")
-    end
-  end
-
-  context 'endpoints have no protocol' do
-    let(:options) do
-      opts = mock_cloud_options['properties']
-      opts['aws'].store("ec2_endpoint", "the_ec2_endpoint.com")
-      opts['aws'].store("elb_endpoint", "the_elb_endpoint.com")
-      opts
-    end
-
-    it 'should configure elb_params' do
-      params = provider.elb_params
-      expect(params[:endpoint]).to eq("https://the_elb_endpoint.com")
-    end
-
-    it 'should configure ec2_params' do
-      params = provider.ec2_params
-      expect(params[:endpoint]).to eq("https://the_ec2_endpoint.com")
+      it { configures_client_with_params }
     end
   end
 
   context 'bosh ca cert file is set' do
     before do
-      ENV['BOSH_CA_CERT_FILE'] = "/some/path/file.pem"
+      @original_bosh_ca_cert_file = ENV['BOSH_CA_CERT_FILE']
+      ENV['BOSH_CA_CERT_FILE'] = '/some/path/file.pem'
+      params[:ssl_ca_bundle] = '/some/path/file.pem'
     end
 
     after do
-      ENV.delete('BOSH_CA_CERT_FILE')
-    end
-    it 'should configure elb_params' do
-      params = provider.elb_params
-      expect(params[:ssl_ca_bundle]).to eq('/some/path/file.pem')
+      if @original_bosh_ca_cert_file.nil?
+        ENV.delete('BOSH_CA_CERT_FILE')
+      else
+        ENV['BOSH_CA_CERT_FILE'] = @original_bosh_ca_cert_file
+      end
     end
 
-    it 'should configure ec2_params' do
-      params = provider.ec2_params
-      expect(params[:ssl_ca_bundle]).to eq('/some/path/file.pem')
-    end
+    it { configures_client_with_params }
   end
 end
