2ad915afc32a2c268835ea80f7abec006cc81685
Joseph Palermo <jpalermo@vmware.com>
2021-04-29 14:13:32 -0700

    Update instances types chosen for vm_resources
    This updates the list of instance types that are chosen from when using
    the vm_resources property. The previous list was about one AWS EC2
    generation out of date. The new list was chosen from general purpose
    instance types that are available across all regions (including US
    GovCloud).
    
    This changes the algorithm for choosing the VM type to be based on
    preference order. The current list is ordered by pricing as of current
    date.
    
    We kept the t2 instance types as they are one of the only 1 CPU options
    available.
    
    [#177650543] **Consider** Updating the BOSH AWS CPI `SUPPORTED_VM_TYPES`
    
    Signed-off-by: Brian Upton <bupton@vmware.com>

--- src/bosh_aws_cpi/lib/cloud/aws/instance_type_mapper.rb
+++ src/bosh_aws_cpi/lib/cloud/aws/instance_type_mapper.rb
@@ -1,49 +1,47 @@
 module Bosh::AwsCloud
   class InstanceTypeMapper
 
-    SUPPORTED_VM_TYPES = [
-      {name: 't2.nano',     cpu: 1,   ram: 0.5 * 1024},
-      {name: 't2.micro',    cpu: 1,   ram: 1 * 1024},
-      {name: 't2.small',    cpu: 1,   ram: 2 * 1024},
-      {name: 'c4.large',    cpu: 2,   ram: 3.75 * 1024},
-      {name: 'm4.large',    cpu: 2,   ram: 8 * 1024},
-      {name: 'r3.large',    cpu: 2,   ram: 15.25 * 1024},
-      {name: 'c4.xlarge',   cpu: 4,   ram: 7.5 * 1024},
-      {name: 'm4.xlarge',   cpu: 4,   ram: 16 * 1024},
-      {name: 'r3.xlarge',   cpu: 4,   ram: 30.5 * 1024},
-      {name: 'c4.2xlarge',  cpu: 8,   ram: 15 * 1024},
-      {name: 'm4.2xlarge',  cpu: 8,   ram: 32 * 1024},
-      {name: 'r3.2xlarge',  cpu: 8,   ram: 61 * 1024},
-      {name: 'c4.4xlarge',  cpu: 16,  ram: 30 * 1024},
-      {name: 'm4.4xlarge',  cpu: 16,  ram: 64 * 1024},
-      {name: 'r3.4xlarge',  cpu: 16,  ram: 122 * 1024},
-      {name: 'x1.32xlarge', cpu: 128, ram: 1952 * 1024},
+    SUPPORTED_VM_TYPES_SORTED_BY_PREFERENCE_ORDER = [
+      { name: 't2.nano',     cpu: 1,  ram: 0.5 * 1024 },
+      { name: 't2.micro',    cpu: 1,  ram: 1 * 1024 },
+      { name: 't2.small',    cpu: 1,  ram: 2 * 1024 },
+      { name: 'c5.large',    cpu: 2,  ram: 4 * 1024 },
+      { name: 'm5.large',    cpu: 2,  ram: 8 * 1024 },
+      { name: 'r5.large',    cpu: 2,  ram: 16 * 1024 },
+      { name: 'c5.xlarge',   cpu: 4,  ram: 8 * 1024 },
+      { name: 'm5.xlarge',   cpu: 4,  ram: 16 * 1024 },
+      { name: 'r5.xlarge',   cpu: 4,  ram: 32 * 1024 },
+      { name: 'c5.2xlarge',  cpu: 8,  ram: 16 * 1024 },
+      { name: 'm5.2xlarge',  cpu: 8,  ram: 32 * 1024 },
+      { name: 'r5.2xlarge',  cpu: 8,  ram: 64 * 1024 },
+      { name: 'c5.4xlarge',  cpu: 16, ram: 32 * 1024 },
+      { name: 'm5.4xlarge',  cpu: 16, ram: 64 * 1024 },
+      { name: 'r5.4xlarge',  cpu: 16, ram: 128 * 1024 },
+      { name: 'c5.9xlarge',  cpu: 36, ram: 72 * 1024 },
+      { name: 'm5.8xlarge',  cpu: 32, ram: 128 * 1024 },
+      { name: 'r5.8xlarge',  cpu: 32, ram: 256 * 1024 },
+      { name: 'c5.12xlarge', cpu: 48, ram: 96 * 1024 },
+      { name: 'm5.12xlarge', cpu: 48, ram: 192 * 1024 },
+      { name: 'r5.12xlarge', cpu: 48, ram: 384 * 1024 },
+      { name: 'm5.16xlarge', cpu: 64, ram: 256 * 1024 },
+      { name: 'r5.16xlarge', cpu: 64, ram: 512 * 1024 }
     ]
 
     def initialize
     end
 
     def map(vm_properties)
-      possible_types = types_meeting_requirement(vm_properties)
-      if possible_types.empty?
-        largest_vm = SUPPORTED_VM_TYPES.last
+      closest_match = SUPPORTED_VM_TYPES_SORTED_BY_PREFERENCE_ORDER.find do |type|
+        type[:cpu] >= vm_properties['cpu'] && type[:ram] >= vm_properties['ram']
+      end
+
+      if closest_match.nil?
+        largest_vm = SUPPORTED_VM_TYPES_SORTED_BY_PREFERENCE_ORDER.last
         raise "Unable to meet requested VM requirements: #{vm_properties['cpu']} CPU, #{vm_properties['ram']} RAM. " +
               "Largest known VM type is '#{largest_vm[:name]}': #{largest_vm[:cpu]} CPU, #{largest_vm[:ram]} RAM."
       end
 
-      closest_match = possible_types.min_by do |type|
-        [type[:cpu], type[:ram]]
-      end
-
       closest_match[:name]
-    end
-
-    private
-
-    def types_meeting_requirement(vm_properties)
-      SUPPORTED_VM_TYPES.select do |type|
-        type[:cpu] >= vm_properties['cpu'] && type[:ram] >= vm_properties['ram']
-      end
     end
   end
 end
--- src/bosh_aws_cpi/spec/unit/instance_type_mapper_spec.rb
+++ src/bosh_aws_cpi/spec/unit/instance_type_mapper_spec.rb
@@ -18,13 +18,20 @@ describe Bosh::AwsCloud::InstanceTypeMapper do
     }
     output_map = {
       tiny_vm => 't2.nano',
-      medium_vm => 'm4.large',
-      large_vm => 'c4.2xlarge',
+      medium_vm => 'm5.large',
+      large_vm => 'c5.2xlarge',
     }
 
     output_map.each do |input, expected_output|
       expect(subject.map(input)).to eq(expected_output)
     end
+  end
+
+  it 'chooses the instance type by preference order' do
+    # Previously we found the instance type based off of the minimal match of CPU, then minimal match of RAM.
+    # For 32 CPU and 64GB of RAM, this normally would have chosen m5.8xlarge. However, the c5.9xlarge is a
+    # closer match that costs less.
+    expect(subject.map({ 'ram' => 64, 'cpu' => 32 })).to eq('c5.9xlarge')
   end
 
   it 'raises an error if no match is found' do
