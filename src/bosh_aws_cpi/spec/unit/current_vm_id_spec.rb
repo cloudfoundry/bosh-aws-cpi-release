1a23fdfc1796c49e8527e0ad53809e2636399db4
Joseph Palermo <jpalermo@vmware.com>
2021-11-08 14:03:57 -0800

    Handle possible edge case where IMDSv2 not supported
    - If we get any error from the token endpoint we assume it is that
      IMDSv2 isn't supported and we try to make the metadata call anyways
      (without the token).
    - It's not clear what a response from a non-IMDSv2 metadata endpoint
      would look like, and we're not even sure they exist. However, were
      this to occur it would cause breakage, whereas this current fallback
      behavior should be safe (although might make debugging IMDS issues
      slightly more difficult.)
    
    Signed-off-by: Rajan Agaskar <ragaskar@vmware.com>

--- src/bosh_aws_cpi/spec/unit/current_vm_id_spec.rb
+++ src/bosh_aws_cpi/spec/unit/current_vm_id_spec.rb
@@ -22,7 +22,7 @@ describe Bosh::AwsCloud::CloudV1 do
     end
 
     let(:fake_instance_id) { 'i-xxxxxxxx' }
-    # TODO IMDS_V2
+
     it 'should make a call to AWS and return the correct vm id' do
       stub_request(:put, 'http://169.254.169.254/latest/api/token')
         .with(headers: { 'X-aws-ec2-metadata-token-ttl-seconds' => '300' })
@@ -33,9 +33,24 @@ describe Bosh::AwsCloud::CloudV1 do
       expect(cloud.current_vm_id).to eq(fake_instance_id)
     end
 
-    it 'should make a call to AWS and return the correct vm id' do
+    it 'should ignore errors from the token endpoint, setting token to nil' do
+      #we thought it made sense to be "defensive" about using IMDSv2 in case there are 
+      #regions that do not support it. Expected behavior if our token call fails is that 
+      #we simply call the instance-id endpoint without the token header and hope for the best. 
       stub_request(:put, 'http://169.254.169.254/latest/api/token')
         .with(headers: { 'X-aws-ec2-metadata-token-ttl-seconds' => '300' })
+        .to_return(status: [500, "Internal Server Error"])
+      stub_request(:get, 'http://169.254.169.254/latest/meta-data/instance-id/')
+        .to_return(:body => fake_instance_id)
+      expect(cloud.current_vm_id).to eq(fake_instance_id)
+    end
+
+    it 'if instance-id endpoint errors, throw a cloud_error' do
+      stub_request(:put, 'http://169.254.169.254/latest/api/token')
+        .with(headers: { 'X-aws-ec2-metadata-token-ttl-seconds' => '300' })
+        .to_return(:body => "token")
+      stub_request(:get, 'http://169.254.169.254/latest/meta-data/instance-id/')
+        .with(headers: { 'X-aws-ec2-metadata-token' => "token" })
         .to_return(status: [500, "Internal Server Error"])
       expect{
         cloud.current_vm_id
