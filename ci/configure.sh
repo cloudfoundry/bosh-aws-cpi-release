251c347a82a122b96ef2656d8a52ee9965ca3a16
Joseph Palermo <jpalermo@pivotal.io>
2022-03-04 12:22:05 -0800

    Update docker pipeline credentials

--- ci/configure.sh
+++ ci/configure.sh
@@ -5,6 +5,5 @@ set -eu
 fly -t bosh-ecosystem sp -p bosh-aws-cpi \
   -c ci/pipeline.yml \
   -l <( lpass show --notes "aws cpi concourse secrets") \
-  -l <( lpass show --notes pivotal-tracker-resource-keys ) \
-  -l <( lpass show --note "bosh:docker-images concourse secrets") \
+  -l <( lpass show --notes pivotal-tracker-resource-keys )
 
