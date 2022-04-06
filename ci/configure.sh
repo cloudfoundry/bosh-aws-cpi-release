42331379f1e4d1fe0caa7184ed6a98503341238e
Joseph Palermo <jpalermo@pivotal.io>
2022-04-05 18:13:56 -0700

    Rotate credentials and rename into user type credentials

--- ci/configure.sh
+++ ci/configure.sh
@@ -4,6 +4,5 @@ set -eu
 
 fly -t bosh-ecosystem sp -p bosh-aws-cpi \
   -c ci/pipeline.yml \
-  -l <( lpass show --notes "aws cpi concourse secrets") \
-  -l <( lpass show --notes pivotal-tracker-resource-keys )
+  -l <( lpass show --notes "aws cpi concourse secrets")
 
