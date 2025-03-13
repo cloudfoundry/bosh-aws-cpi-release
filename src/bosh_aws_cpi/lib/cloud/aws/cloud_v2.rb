fe51065e0da98e128eb424fbbd9ac2717b76fe07
Dennis Ahaus <dennis.ahaus@sap.com>
2025-03-13 16:46:52 +0100

    Bump api version

--- src/bosh_aws_cpi/lib/cloud/aws/cloud_v2.rb
+++ src/bosh_aws_cpi/lib/cloud/aws/cloud_v2.rb
@@ -8,7 +8,7 @@ module Bosh::AwsCloud
     DEVICE_POLL_TIMEOUT = 60 # in seconds
 
     # Current CPI API version supported by this CPI
-    API_VERSION = 2
+    API_VERSION = 3
 
     # Stemcell API version that first supports optional-registry operation
     STEMCELL_NO_REGISTRY = 2
