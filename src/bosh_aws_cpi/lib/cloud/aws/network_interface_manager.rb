7889c8f241a8f139e46d1dd4e785adaca7730236
Ansh Rupani <ansh.rupani@sap.com>
2025-08-22 10:30:13 +0200

    refactor the network interfaces and nic_group approach in the aws cpi

---
+++ src/bosh_aws_cpi/lib/cloud/aws/network_interface_manager.rb
@@ -0,0 +1,101 @@
+module Bosh::AwsCloud
+  class NetworkInterfaceManager
+    include Helpers
+
+    CREATE_NETWORK_INTERFACE_WAIT_TIME = 30
+
+    def initialize(ec2_client, logger, security_group_mapper)
+      @ec2_client = ec2_client
+      @logger = logger
+      @security_group_mapper = security_group_mapper
+    end
+
+    def provision_network_interfaces(nic_groups)
+      network_interfaces = []
+      nic_groups.each_value do |nic_group|
+        # Get subnet from the nic_group
+        subnet_id_val = nic_group.subnet_id
+        
+        # Get security groups
+        sg = @security_group_mapper.map_to_ids(security_groups(network_cloud_props, vm_type, default_security_groups), subnet_id_val)
+
+        if ( sg.nil? || sg.empty? )
+          raise Bosh::Clouds::CloudError, "Missing security groups. See http://bosh.io/docs/aws-cpi.html for the list of supported properties."
+        end
+
+        nic = {}
+        nic[:groups] = sg unless sg.nil? || sg.empty?
+        nic[:subnet_id] = subnet_id_val
+        
+        # Only populate addresses if the nic_group contains manual networks
+        if nic_group.manual_network?
+          nic[:ipv_6_addresses] = [{ ipv_6_address: nic_group.ipv6_address }] if nic_group.has_ipv6_address?
+          nic[:private_ip_address] = nic_group.ipv4_address if nic_group.has_ipv4_address?
+        end
+        
+        # Get prefixes from nic_group
+        prefixes = nic_group.prefixes
+
+        errors = [Aws::EC2::Errors::InvalidIPAddressInUse]
+        @logger.info("Creating new network_interface with: #{nic.inspect}")
+        Bosh::Common.retryable(sleep: network_interface_create_wait_time, tries: 20, on: errors) do |_tries, error|
+          @logger.info('Launching network interface...')
+          @logger.warn("IP address was in use: #{error}") if error.is_a?(Aws::EC2::Errors::InvalidIPAddressInUse)
+
+          resp = @ec2_client.client.create_network_interface(nic)
+          network_interface_id = get_created_network_interface_id(resp)
+          network_interface = Bosh::AwsCloud::NetworkInterface.new(@ec2_client.network_interface(network_interface_id), @ec2_client.client, @logger)
+          network_interface.wait_until_available
+          network_interface.attach_ip_prefixes(prefixes) unless prefixes.nil?
+          network_interface.add_associate_public_ip_address(vm_cloud_props)
+          nic_group.assign_mac_address(network_interface.mac_address)
+          network_interfaces.append(network_interface)
+        end
+        network_interfaces
+      end
+    end
+
+    def create_network_interfaces(network_cloud_props, vm_type, default_security_groups)
+      # Iterate over network cloud props and group networks by nic_group
+      nic_groups = {}
+      first_dynamic_network = nil
+      
+      networks_cloud_props.networks.each do |network|
+        if network.type == 'manual'
+          nic_group_name = network.nic_group
+          nic_groups[nic_group_name] ||= Bosh::AwsCloud::NicGroup.new(nic_group_name)
+          nic_groups[nic_group_name].add_network(network)
+        elsif network.type == 'dynamic'
+          # Capture the first dynamic network encountered
+          first_dynamic_network ||= network
+        end
+      end
+      
+      # Add dynamic network to the nic_group structure if one was found
+      if first_dynamic_network
+        nic_groups[first_dynamic_network.name] = Bosh::AwsCloud::NicGroup.new(first_dynamic_network.name, [first_dynamic_network])
+      end
+
+      provision_network_interfaces(nic_groups)
+    end
+
+    private
+
+    def get_created_network_interface_id(resp)
+      resp.network_interface.network_interface_id
+    end
+
+    def security_groups(networks_cloud_props, vm_type, default_security_groups)
+      networks_security_groups = networks_cloud_props.security_groups
+
+      groups = default_security_groups
+      groups = networks_security_groups unless networks_security_groups.empty?
+      groups = vm_type.security_groups unless vm_type.security_groups.empty?
+      groups
+    end
+
+    def network_interface_create_wait_time
+      Bosh::AwsCloud::NetworkInterface::CREATE_NETWORK_INTERFACE_WAIT_TIME
+    end
+  end
+end
