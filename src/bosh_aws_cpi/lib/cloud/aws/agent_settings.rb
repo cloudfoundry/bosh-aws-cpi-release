7865054b0b3e905cc0648774f51da6e576caa229
Ansh Rupani <ansh.rupani@sap.com>
2025-08-27 10:30:48 +0200

    cleanup leftovers

--- src/bosh_aws_cpi/lib/cloud/aws/agent_settings.rb
+++ src/bosh_aws_cpi/lib/cloud/aws/agent_settings.rb
@@ -3,7 +3,6 @@ module Bosh::AwsCloud
 
     attr_accessor :agent_disk_info, :agent_config, :root_device_name
     attr_accessor :agent_id, :environment
-    attr_reader :networks
 
     # Generates initial agent settings. These settings will be read by agent
     # from AWS registry (also a BOSH component) on a target instance. Disk
@@ -71,7 +70,6 @@ module Bosh::AwsCloud
     end
 
     private
-
     def agent_network_spec(networks_cloud_props)
       spec = networks_cloud_props.networks.map do |net|
         settings = net.to_h
